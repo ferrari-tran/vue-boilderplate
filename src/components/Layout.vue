ac014b795fe28b672a3f236801d8494e53de9a5d
Lao Ton <laoton@Laos-MacBook-Pro.local>
2018-02-06 17:36:45 +0700

    add at-components framework and set sass lang

---
+++ src/components/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <section class="cs-navbar">
+      Navbar
+    </section>
+    <main class="cs-main">
+      Main
+    </main>
+  </div>
+</template>
+
+<style lang="scss">
+  $primary-color: #08085E;
+  .cs-navbar {
+    background: $primary-color;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import AtComponents from 'at-ui'
+import 'at-ui-style' // Import CSS
+
+import './assets/sass/style.scss'
+
+Vue.config.productionTip = false
+Vue.use(AtComponents)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
